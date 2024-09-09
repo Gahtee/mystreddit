0a658b78bc08bd16d6347cc8c9868ac27aa4ddc7
Gabriel Timóteo <gabrieltimoteopessoal@gmail.com>
2024-09-09 20:49:24 -0300

    foi o que deu

--- index.js
+++ index.js
@@ -46,70 +46,66 @@ for (var num = reddit.length-1; num != -1; num --) {
 */
 
 (async () => {
-    var redd = `https://www.reddit.com/r/${reddit[0]}/top.rss?t=day`
-    var rss = await parser.parseURL(redd);
-    var sub = rss.items[0].content.split("/r/")[1].split("/")[0];
-    var id = []
-    for (var it = 0; it != 10; it++) {
-        id.push(rss.items[it].content.split("/comments/")[1].split("/")[0]); //melhorar isso; lembrar de alterar o 0 por it
-    }
-    for (var it = id.length-1; it != -1; it--) {
-        if (isnew(sub, id[it])) {
-            //função para checar se é video
-        }        
-        else {
-            console.log("não é novo")
+    for (var num = reddit.length-1; num != -1; num --) {
+        var redd = `https://www.reddit.com/r/${reddit[num]}/hot.rss`
+        console.log(redd)
+        var rss = await parser.parseURL(redd);
+        var nen = 0
+        var sub = rss.items[0].content.split("/r/")[1].split("/")[0];
+        var id = []
+        if (fs.existsSync(`./database/${sub}.txt`)) {
+            var antigo = fs.readFileSync(`./database/${sub}.txt`).toString(); //me parece uma péssima maneira de implementar isso, dupla verificação se um arquivo existe.
+        }
+        for (var it = 0; it != 10; it++) {
+            id.push(rss.items[it].content.split("/comments/")[1].split("/")[0]);
+        }
+        for (var it = id.length-1; it != -1; it--) {
+            if (top10(sub, id[it])) {
+                console.log("é novo", id[it])
+                //função para checar se é video
+            }        
+            else {
+                nen++
+            }
+        }
+        console.log(nen)
+        if (nen != 10) {
+            var antigo = fs.readFileSync(`./database/${sub}.txt`).toString().split(" ",7);
+            if (!antigo.includes(id))
+                fs.writeFileSync(`./database/${sub}.txt`, id.toString())
         }
     }
-
-console.log(id) 
-
-
 })();
 
-function isnew (sub, id) { //checa se tem id novo
-    if (fs.existsSync(`./database/${sub}.json`)) {
-        var ids = JSON.parse(fs.readFileSync(`./database/${sub}.json`, 'utf8'));
+function top10 (sub, id) { //escreve todos os ids do top10 em um txt
+    if (fs.existsSync(`./database/${sub}.txt`)) {
+        var ids = fs.readFileSync(`./database/${sub}.txt`).toString();
         if (ids.includes(id)) {
             return false;
         }
         else {
-            console.log(id)
-            var data = JSON.parse(fs.readFileSync(`./database/${sub}.json`));
-            data.push(","+) //travei
-            fs.writeFileSync(`./database/${sub}.json`, id)
+            var data = fs.readFileSync(`./database/${sub}.txt`).toString().split(" ",7);
+            data.push(id);
+            //console.log(data)
+            fs.writeFileSync(`./database/${sub}.txt`, data.toString())
             return true
         }
     }
     else {
-        console.log(id)
-        fs.writeFileSync(`./database/${sub}.json`, JSON.stringify(id))
+        fs.writeFileSync(`./database/${sub}.txt`, id)
         return true
     }
 }
-/*
-esta é uma maneira burra de se fazer isso, mas funciona.
-*/
+//esta é uma maneira burra de se fazer isso, mas funciona.
 
 
-async function readlist (sub) { //checa e retorna todos os os ids no json
-    if (fs.existsSync(`./database/${sub}.json`)) {
-        var ids = JSON.parse(fs.readFileSync(`./database/${sub}.json`, 'utf8'));
-            return ids;
-    }
-    else {
-        fs.writeFileSync(`./database/${sub}.json`, '[]', )
-        var ids = [];
-        return ids;
-    }
-}
 //---------ATENÇÃO---------
 //são 3 etapas, checar antigos, mandar novos, deletar antigos
 //acabei perdendo um pouco o rumo, preciso fazer uma variavel contendo todos os ids dos top10 post do reddit
 //1 etapa: ver tudo o que tem na lista atual - OK
-//2 etapa: ver como está a nova lista (atualizada)
+//2 etapa: ver como está a nova lista (atualizada) - OK
 //3 etapa: manda os novos itens para o discord
-//4 etapa: apagar todos os itens que não forem novos (pode ser feito apagando tudo e sobrescrevendo)
+//4 etapa: apagar todos os itens que não forem novos (pode ser feito apagando tudo e sobrescrevendo) - OK
 //---------ATENÇÃO---------
 
 
