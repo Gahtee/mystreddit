7c974c7ea19a5dc6fe81cac33d3f0c4c37c969f3
Gabriel Timóteo <gabrieltimoteopessoal@gmail.com>
2024-09-09 12:08:13 -0300

    começando  Changes to be committed: 	modified:   .env.example 	modified:   .gitignore 	modified:   index.js 	new file:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-pau
+//Consts
+const dotenv = require('dotenv')
+dotenv.config();
+
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const token = process.env.TOKEN
+
+//Código de iniciação
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+],
+});
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Iniciado! logado como ${readyClient.user.tag}`);
+});
+
+client.login(token);
+
