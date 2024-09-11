d2b418c2c00920e9942f54b835bfa7b2369de6aa
Gabriel Timóteo <gabrieltimoteopessoal@gmail.com>
2024-09-11 18:44:00 -0300

    try catch, detecção de midia e deteção melhorada de imgem

--- index.js
+++ index.js
@@ -41,74 +41,82 @@ const reddit = env.reddit.split(",");
 //usa-se como ultimo o id da ultima postagem
 
 async function redd() {
-    while (true) {
-        for (var num = reddit.length-1; num != -1; num --) {
-            var redd = `https://www.reddit.com/r/${reddit[num]}/hot.rss`;
-            var rss = await parser.parseURL(redd)
-            var nen = 0
-            var sub = rss.items[0].content.split("/r/")[1].split("/")[0];
-            var id = []
-            if (fs.existsSync(`./database/${sub}.txt`)) {
-                var antigo = fs.readFileSync(`./database/${sub}.txt`).toString(); //me parece uma péssima maneira de implementar isso, dupla verificação se um arquivo existe.
-            }
-            for (var it = 0; it != 10; it++) {
-                id.push(rss.items[it].link);
-            }
-            for (var it = id.length-1; it != -1; it--) {
-                if (top10(sub, id[it])) {
-                    var author = rss.items[it].author;
-                    var title = rss.items[it].title.toString();
-                    var img = rss.items[it].content.split("<span><a href=\"")[1].split("\">[link]")[0];
-                    if (!rss.entry['media:thumbnail']) {
-                        const embed = new EmbedBuilder()
-                            .setColor(0xFF5700)
-                            .setTitle(title)
-                            .setURL(id[it])
-                            .setAuthor({ name: `${author}`, url: `https://www.reddit.com${author}` })
-                            .setThumbnail('https://www.iconpacks.net/icons/2/free-reddit-logo-icon-2436-thumb.png')
-                            .setImage(img)
-                        client.channels.cache.get(env.channelid).send({ embeds: [embed] });
-                    } else {
-                        console.log('foi')
-                        client.channels.cache.get(env.channelid).send(`&download ${id[it]} | ${sub} | ${author} | ${title}`)
-                        .then(sentMessage => {
-                            setTimeout(() => {
-                                sentMessage.delete();
-                            }, 5000); // 5 segundos
-                        })
-
+    try {
+        while (true) {
+            for (var num = reddit.length-1; num != -1; num --) {
+                var redd = `https://www.reddit.com/r/${reddit[num]}/hot.rss`;
+                var rss = await parser.parseURL(redd)
+                var nen = 0
+                var sub = rss.items[0].content.split("/r/")[1].split("/")[0];
+                var id = []
+                if (fs.existsSync(`./database/${sub}.txt`)) {
+                    var antigo = fs.readFileSync(`./database/${sub}.txt`).toString(); //me parece uma péssima maneira de implementar isso, dupla verificação se um arquivo existe.
+                } else {
+                    fs.openSync(`./database/${sub}.txt`, 'w+')
+                }
+                for (var it = 0; it != 10; it++) {
+                    if (rss.items[it].content.includes('src')){
+                        id.push(rss.items[it].link);
                     }
-                }        
-                else {
-                    nen++
+                }
+                for (var it = id.length-1; it != -1; it--) {
+                    if (top10(sub, id[it])) {
+                        var author = rss.items[it].author;
+                        var title = rss.items[it].title.toString();
+                        var img = rss.items[it].content.split("<span><a href=\"")[1].split("\">[link]")[0];
+                        if (!rss.entry['media:thumbnail'][it]) {
+                            const embed = new EmbedBuilder()
+                                .setColor(0xFF5700)
+                                .setTitle(title)
+                                .setURL(id[it])
+                                .setAuthor({ name: `${author}`, url: `https://www.reddit.com${author}` })
+                                .setThumbnail('https://www.iconpacks.net/icons/2/free-reddit-logo-icon-2436-thumb.png')
+                                .setImage(img)
+                            client.channels.cache.get(env.channelid).send({ embeds: [embed] });
+                        } else {
+                            console.log('foi')
+                            client.channels.cache.get(env.channelid).send(`&download ${id[it]} | ${sub} | ${author} | ${title}`)
+                            .then(sentMessage => {
+                                setTimeout(() => {
+                                    sentMessage.delete();
+                                }, 5000); // 5 segundos
+                            })
+
+                        }
+                    }
+                    else {
+                        nen++
+                    }
+                }
+                if (nen != 10) {
+                    var antigo = fs.readFileSync(`./database/${sub}.txt`).toString().split(" ",7);
+                    if (!antigo.includes(id))
+                        fs.writeFileSync(`./database/${sub}.txt`, id.toString())
                 }
             }
-            if (nen != 10) {
-                var antigo = fs.readFileSync(`./database/${sub}.txt`).toString().split(" ",7);
-                if (!antigo.includes(id))
-                    fs.writeFileSync(`./database/${sub}.txt`, id.toString())
+        await new Promise(resolve => setTimeout(resolve, 60000)); // espera por 60 segundos
+        console.log('Rodando...');
+        }
+    }  catch (e) {
+        console.log(e)
+    }
+}
+    function top10 (sub, id) { //escreve todos os ids do top10 em um txt
+        if (fs.existsSync(`./database/${sub}.txt`)) {
+            var ids = fs.readFileSync(`./database/${sub}.txt`).toString();
+            if (ids.includes(id)) {
+                return false;
+            }
+            else {
+                var data = fs.readFileSync(`./database/${sub}.txt`).toString().split(" ",7);
+                data.push(id);
+                fs.writeFileSync(`./database/${sub}.txt`, data.toString())
+                return true
             }
         }
-      await new Promise(resolve => setTimeout(resolve, 60000)); // espera por 60 segundos
-      console.log('Rodando...');
-    }
-  }
-
-function top10 (sub, id) { //escreve todos os ids do top10 em um txt
-    if (fs.existsSync(`./database/${sub}.txt`)) {
-        var ids = fs.readFileSync(`./database/${sub}.txt`).toString();
-        if (ids.includes(id)) {
-            return false;
-        }
         else {
-            var data = fs.readFileSync(`./database/${sub}.txt`).toString().split(" ",7);
-            data.push(id);
-            fs.writeFileSync(`./database/${sub}.txt`, data.toString())
+            fs.writeFileSync(`./database/${sub}.txt`, id)
             return true
         }
-    }
-    else {
-        fs.writeFileSync(`./database/${sub}.txt`, id)
-        return true
-    }
+
 } //Parece existir maneiras melhores de fazer isso.
